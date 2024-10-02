d5e215121f3708a401fb7bbf7d08a93f1ae51d4a
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-02 14:22:12 -0400

    Chapter example part 2 added

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Divide(10, 2);
+        Divide(10,0);
+
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +16,43 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for(String student : studentFiles.keySet()) {
+            String fileName = studentFiles.get(student);
+            try {
+                int points = CheckFileExtension(fileName);
+                System.out.println(student + " receives " + points +" points");
+            } catch (Exception e) {
+                System.out.println(student + " receives 444444 points Error exemption " + e.getMessage());
+            }
+        }
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        try {
+            int result = x / y;
+            System.out.println("Result: " + result);
+            return result;
+        } catch (ArithmeticException e) {
+            System.out.println("Exeption message: Division by zero!");
+            return 4444;
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+
+    public static int CheckFileExtension(String fileName) {
+        try {
+            if (fileName == null || fileName.isEmpty()) {
+                throw new Exception("File name is null or empty!");
+        }
+            if (fileName.endsWith(".java")) {
+                return 1;
+            } else {
+                return 0;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 4444444;
+            }
+        }
     }
-}
